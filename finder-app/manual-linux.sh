c303f8ba1bbb9ce53f3fe5b6995610930c88199e
vboxuser <beaspin@icloud.com>
2025-02-27 12:35:23 +0100

    Updated

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -5,7 +5,7 @@
 set -e
 set -u
 
-OUTDIR=/tmp/aeld
+OUTDIR=${1:-/tmp/aeld}
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
@@ -13,84 +13,104 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-linux-gnu-
 
-if [ $# -lt 1 ]
-then
-	echo "Using default directory ${OUTDIR} for output"
-else
-	OUTDIR=$(realpath $1)
-	echo "Using passed directory ${OUTDIR} for output"
-fi
+echo "Using OUTPUT directory: ${OUTDIR}"
+ABS_OUTDIR=$(realpath "${OUTDIR}")
 
-mkdir -p ${OUTDIR} || { echo "Failed to create output directory"; exit 1; }
+mkdir -p ${ABS_OUTDIR} || { echo "Failed to create output directory"; exit 1; }
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+REQUIRED_TOOLS=("make" "gcc" "git" "wget" "tar" "${CROSS_COMPILE}gcc" "cpio")
+for tool in "${REQUIRED_TOOLS[@]}"; do
+    if ! command -v "$tool" &>/dev/null; then
+        echo "Error: $tool is not installed. Please install it and retry."
+        exit 1
+    fi
+done
+
+cd "${ABS_OUTDIR}"
+if [ ! -d "linux-stable" ]; then
+    echo "Cloning Linux kernel source..."
+    git clone --depth 1 --single-branch --branch ${KERNEL_VERSION} ${KERNEL_REPO} linux-stable
 fi
 
 cd linux-stable
 echo "Checking out version ${KERNEL_VERSION}"
 git checkout ${KERNEL_VERSION}
 
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    echo "Building the kernel"
+if [ ! -e ${ABS_OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+    echo "Building the kernel..."
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
-cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
+cp arch/${ARCH}/boot/Image "${ABS_OUTDIR}/"
 
-echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+ROOTFS_DIR="${OUTDIR}/rootfs"
+if [ -d "${ROOTFS_DIR}" ]; then
+    echo "Cleaning existing rootfs directory..."
+    sudo rm  -rf "${ROOTFS_DIR}"
 fi
 
-mkdir -p ${OUTDIR}/rootfs/{bin,sbin,etc,proc,sys,usr/bin,usr/sbin,lib,lib64,dev,home,tmp,var,root}
+mkdir -p "${ROOTFS_DIR}"/{bin,sbin,etc,proc,sys,usr/bin,usr/sbin,lib,lib64,dev,home,tmp,var,root}
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
-    make distclean
-    make defconfig
-else
-    cd busybox
+cd "${ABS_OUTDIR}"
+if [ ! -d "busybox" ]; then
+    echo "Cloning BusyBox..."
+    git clone git://busybox.net/busybox.git
 fi
 
+cd busybox
+git fetch --tags
+git checkout master
+make distclean
+make defconfig
 make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX="${ROOTFS_DIR}" install
 
-echo "Library dependencies"
-SYSROOT=/usr/aarch64-linux-gnu
-mkdir -p ${OUTDIR}/rootfs/lib
-mkdir -p ${OUTDIR}/rootfs/lib64
-cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
-cp -a ${SYSROOT}/lib/libm.so.6 ${OUTDIR}/rootfs/lib/
-cp -a ${SYSROOT}/lib/libresolv.so.2 ${OUTDIR}/rootfs/lib/
-cp -a ${SYSROOT}/lib/libc.so.6 ${OUTDIR}/rootfs/lib/
+echo "Adding library dependencies..."
+SYSROOT="/usr/aarch64-linux-gnu/"
 
-sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
-sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
+if [ -d "${SYSROOT}/lib" ]; then
+    LIB_DIR="${SYSROOT}/lib"
+elif [ -d "${SYSROOT}/lib64" ]; then
+    LIB_DIR="${SYSROOT}/lib64"
+else
+    echo "Error: No valid lib directory found in ${SYSROOT}"
+    exit 1
+fi
+echo "Checking directories..."
+ls -ld "$ROOTFS_DIR"
+ls -ld "$ROOTFS_DIR/lib"
 
-cd ${FINDER_APP_DIR}
+mkdir -p "${ROOTFS_DIR}/lib"
+mkdir -p "${ROOTFS_DIR}/lib64"
+cp -a ${LIB_DIR}/ld-linux-aarch64.so.1 "${ROOTFS_DIR}/lib/"
+cp -a ${LIB_DIR}/libm.so.6 "${ROOTFS_DIR}/lib/"
+cp -a ${LIB_DIR}/libresolv.so.2 "${ROOTFS_DIR}/lib/"
+cp -a ${LIB_DIR}/libc.so.6 "${ROOTFS_DIR}lib/"
+
+echo "Creating device nodes..."
+sudo mknod -m 666 "${ROOTFS_DIR}/dev/null" c 1 3
+sudo mknod -m 600 "${ROOTFS_DIR}/dev/console" c 5 1
+
+cd "${FINDER_APP_DIR}"
 make clean
 make CROSS_COMPILE=${CROSS_COMPILE}
-mkdir -p ${OUTDIR}/rootfs/home
-cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
-cp ${FINDER_APP_DIR}/conf/username.txt ${OUTDIR}/rootfs/home/
-cp ${FINDER_APP_DIR}/conf/assignment.txt ${OUTDIR}/rootfs/home/
-cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
-sed -i 's|../conf/assignment.txt|conf/assignment.txt|' ${OUTDIR}/rootfs/home/finder-test.sh
-cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
 
-sudo chown -R root:root ${OUTDIR}/rootfs
+echo "Copying Finder App and scripts..."
+mkdir -p "${ROOTFS_DIR}/home"
+cp "${FINDER_APP_DIR}/writer" "${ROOTFS_DIR}/home/"
+cp "${FINDER_APP_DIR}/conf/username.txt" "${ROOTFS_DIR}/home/"
+cp "${FINDER_APP_DIR}/conf/assignment.txt" "${ROOTFS_DIR}/home/"
+cp "${FINDER_APP_DIR}/finder-test.sh" "${ROOTFS_DIR}/home/"
+sed -i 's|../conf/assignment.txt|conf/assignment.txt|' "${ROOTFS_DIR}/home/finder-test.sh"
+cp "${FINDER_APP_DIR}/autorun-qemu.sh" "${ROOTFS_DIR}/home/"
 
-cd ${OUTDIR}/rootfs
-find . | cpio -o --format=newc | gzip > ${OUTDIR}/initramfs.cpio.gz
+echo "Setting root owenship for rootfs..."
+sudo chown -R root:root "${ROOTFS_DIR}"
+
+echo "Creating initramfs..."
+cd "${ROOTFS_DIR}"
+find . | cpio -o --format=newc | gzip > "${ABS_OUTDIR}/initramfs.cpio.gz"
+
+echo "Kernel and root filesystem build complete!"
